680c9dfc56cba28e0f88bc9eca96345a3440bde7
DESKTOP-R04EAQD\gh423 <gh423gh@gmail.com>
2022-02-06 16:39:23 +0900

    flutter Drawer 메뉴 연습

--- lib/main.dart
+++ lib/main.dart
@@ -1,138 +1,108 @@
 import 'package:flutter/material.dart';
 
-//컴파일할 때 main함수를 가장 먼저 참조함
-//=> 코딩을 간결하게 하기 위한 기호 여기에서는 main함수가 다른 함수를 호출한다.
-//runApp : 최상위 함수 (반드시 위젯을 argument로 가져야 함)
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyPage(),
       debugShowCheckedModeBanner: false,
-      title: 'BBANTO',
-      home: Grade(),//app이 정상적으로 실행되었을 때 가장 먼저 화면에 보여주는 경로
     );
   }
 }
 
-class Grade extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {//다른 위젯을 리턴하는 역할
-    return Scaffold(
-      backgroundColor: Colors.amber[800],
-      appBar: AppBar(
-        title: Text('BBANTO'),
-        centerTitle: true,
-        backgroundColor: Colors.amber[700],
-        elevation: 0.0,//app bar 입체감 설정
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/KakaoTalk_20211204_163516637_01.jpg'),
-                radius: 60.0,
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appbar icon menu'),
+        centerTitle: true,
+        elevation: 0.0,
+
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: (){
+              print('Shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              print('Search button is clicked');
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/KakaoTalk_20211204_163516637_01.jpg')
               ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-            Text('NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('BBANTO',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold
-            ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text('BBANTO POWER LEVEL',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('14',
-              style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/KakaoTalk_20211204_213736845_02.jpg')
                 ),
-                Text('using lightsaber',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                ),
-                ),
+                // CircleAvatar(
+                //     backgroundImage: AssetImage('assets/KakaoTalk_20211204_213736845_02.jpg')
+                // )
               ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('face hero tattoo',
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('fire flames',
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-                  ),
-                ),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/KakaoTalk_20211204_213736845_02.jpg'),
-                radius: 40.0,
-                backgroundColor: Colors.amber[800],
+              accountName: Text('BBANTO'),
+              accountEmail: Text('bbanto@bbanto.com'),
+              onDetailsPressed: (){
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0)
+                )
               ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: Text('Home'),
+              onTap: (){
+                print('Home is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text('Setting'),
+              onTap: (){
+                print('Setting is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: Text('Q&A'),
+              onTap: (){
+                print('Q&A is clicked');
+              },
+              trailing: Icon(Icons.add),
             )
           ],
         ),
